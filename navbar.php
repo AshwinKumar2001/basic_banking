773704ff2aa47f0a78e06aa04c0dda93f7ddfa31
AshwinKumar2001 <techgod2001@gmail.com>
2022-06-18 14:50:06 +0100

    Additional Changes

--- navbar.php
+++ navbar.php
@@ -1,4 +1,5 @@
 <!-- navbar --> 
+      <link rel="stylesheet" type="text/css" href="css/newnav.css">
       <nav class="navbar navbar-expand-md navbar-light bg-light">
       <a class="navbar-brand" href="index.php">Andromeda Bank</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
